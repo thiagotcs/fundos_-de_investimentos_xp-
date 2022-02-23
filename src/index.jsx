2dbe748b02f09817e86a413e7c2b12a51d907b46
thiagotcs <thiagotcs.dev@gmail.com>
2022-02-23 16:05:58 -0300

    chore: add config eslint-plugin-prettier

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+import { HomeView } from './views/Home.view';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeView />
   </React.StrictMode>,
   document.getElementById('root')
 );
